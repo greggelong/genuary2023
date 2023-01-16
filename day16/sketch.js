09b2559c4707297568811d01c078f6a7aceaad40
Greg K <gkreisman_cep@qq.com>
2023-01-16 15:12:39 +0100

    center canvas

--- day16/sketch.js
+++ day16/sketch.js
@@ -1,73 +1,70 @@
+// using rotate and scale function
+// to do the reflection across x and y
+// the https://p5js.org/examples/interaction-kaleidoscope.html  example
+// in p5 shows it well
 
-//The grid is an 2d array of angles. The brightness of the cell is mapped to the sine of the angle in that cell. Then the angle of each cell is incremented by a small random velocity, so the original pattern slowly disappears.
+let symmetry = 6;
+let angle = 360 / symmetry;
+let img;
+let x = 0;
+let sz = 50;
+let theta = 0;
+let cnv
 
-let grid = [];
-let nextGrid = [];  // not using this one
-let cnv;
-let gsz = 60;
-let sz = 20;
+
 
 function setup() {
-  cnv = createCanvas(600, 600);
-  let cx = (windowWidth - cnv.width) / 2;
-  let cy = (windowHeight - cnv.height) / 2;
-  cnv.position(cx, cy);
-  makeGrid();
-  //showGrid();
-  //update();
-  //showGrid();
-  //frameRate(5)
-  noStroke();
-  angleMode(DEGREES)
-  //frameRate(15)
+  cnv =createCanvas(800, 800);
+  let cx = (windowWidth-cnv.width)/2
+  let cy = (windowHeight-cnv.height)/2
+  cnv.position(cx,cy)
+
+  angleMode(DEGREES);
+  background(0);
+  img= createCapture(VIDEO)
+  img.hide()
+  img.size(150,150)
+ // plotmirror();
+  //imageMode(CENTER);
+  frameRate(10)
+  
 }
 
-function draw(){
-  background(0)
-  showGrid();
+function draw() {
+  //background(0)
+  
+  plotmirror();
+ // print(theta)
+  x += sz;
+  x=x%400
+  
+  
+}
+  
+function plotmirror() {
+  push();
+ translate(width / 2, height / 2);
+
+  for (let i = 0; i < symmetry; i++) {
+    rotate(angle);
+    tint(255,255-(x/2))
+    image(img, -x, -x, x / 2, x / 2);
+    push();
+    scale(1, -1);
+    image(img, -x, -x, x / 2, x / 2);
+    pop();
+  }
+  pop();
 }
 
-function makeGrid() {
-  let c = floor(random(3))
-  
-  for (let j = 0; j < gsz; j++) {
-    grid[j] = [];
-    for (let i = 0; i < gsz; i++) {
-     
-      if (c === 0) grid[j][i]= ((j)*40)%360
-      else if (c === 1) grid[j][i]= ((i)*40)%360
-      
-      else if (c === 2) grid[j][i]= ((i+j)*40)%360
-      
-    }
-  }
-}
- 
- 
-function showGrid() {
-  for (let j = 0; j < gsz; j++) {
-    for (let i = 0; i < gsz; i++) {
-    
-        let a = map(sin(grid[j][i]),-1,1,0,255)
-  
-        fill(0,255,0,a)
-      rect(i * sz, j * sz, sz, sz);
-      // increace  angle in by 1 or random 0,3 or velocity
-      grid[j][i]+=floor(random(0,6))
-     // print(grid[j][i])
-      if(grid[j][i]>360) grid[j][i] =0
-      //grid[j][i] = grid[j[i]]%360
-    }
-  }
-}
 
 function keyPressed() {
   // this will download the first 25 seconds of the animation!
   if (key === 'g') {
-    saveGif('sineW.gif', 15);
+    saveGif('reflection.gif', 15);
   }
   if (key === 's') {
-    saveCanvas('sineW', 'jpg');
+    saveCanvas('reflection', 'jpg');
   }
   
 }
